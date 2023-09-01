836a1b2f1e2213aa6de4372d128d9ccd0083d1fc
thiago da silva <thg021@gmail.com>
2023-09-01 19:39:59 -0300

    feat: adicionado layout do header

---
+++ src/app/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+export const Logo = () => (
+  <Image
+    src='/logo.svg'
+    alt='Logo'
+    width={90}
+    height={90}
+    className='cursor-pointer'
+  />
+);
