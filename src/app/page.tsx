b5be2ebb706ae3c77c76128fb2265e475e1b4fc0
Thiago Silva <thiago.silva>
2024-09-30 22:23:35 -0300

    feat: add movies row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,16 @@
-import Image from 'next/image';
-import Header from './components/Header';
-import { InformationCircleIcon, PlayIcon } from '@heroicons/react/16/solid';
+import { MovieRow } from './components/MovieRow';
+import { Banner } from './components/Banner';
 
 export default function Home() {
   return (
-    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
-      <Header />
-      <main className='relative pb-24 pl-4 lg:pl-16 '>
-        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col'>
-            <Image
-              src='/banner.png'
-              alt='MAID'
-              fill={true}
-              className='h-[65vh] object-cover object-top lg:h-[95vh]'
-            />
-          </div>
+    <main className='relative pb-24 pl-4 lg:pl-16'>
+      <Banner />
+      <MovieRow sectionTitle='Sessão 1' />
+      <MovieRow sectionTitle='Sessão 2' />
 
-          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
-            The Witcher
-          </h1>
-          <p className='text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
-            After fleeing an abusive relationship, a young mother finds a job
-            cleaning houses as she fights to provide for her child and build
-            them a better future.
-          </p>
-          <div className='flex space-x-3'>
-            <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-              <PlayIcon className='h-6' aria-label='play no video' />
-              Play
-            </button>
-            <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-white transition hover:opacity-75 md:px-8 md:py-2.5'>
-              <InformationCircleIcon className='h-6' />
-              More Info
-            </button>
-          </div>
-        </div>
-      </main>
-    </div>
+      <MovieRow sectionTitle='Sessão 2' />
+
+      <MovieRow sectionTitle='Sessão 2' />
+    </main>
   );
 }
